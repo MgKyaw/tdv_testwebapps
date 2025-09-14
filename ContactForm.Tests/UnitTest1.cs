188e6f30c743f1707945c73678418fc8ea8eceed
MgKyaw <mgkyaw20112@gmail.com>
2025-09-14 14:56:06 +0700

    feat: Use TestRunParameter

--- ContactForm.Tests/UnitTest1.cs
+++ ContactForm.Tests/UnitTest1.cs
@@ -4,10 +4,19 @@ using System.Text.RegularExpressions;
 [Parallelizable(ParallelScope.Self)]
 public class Tests : PageTest
 {
+    public static string webAppUrl;
+
+    [OneTimeSetUp]
+    public void Init()
+    {
+        webAppUrl = TestContext.Parameters["WebAppUrl"]
+                ?? throw new Exception("WebAppUrl is not configured as a parameter.");
+    }
+
     [Test]
     public async Task Clicking_ContactButton_Goes_To_ContactForm()
     {
-        await Page.GotoAsync("http://localhost:5108");
+        await Page.GotoAsync(webAppUrl);
         var formButton = Page.Locator("text=Open Contact Form");
         await formButton.ClickAsync();
         await Expect(Page).ToHaveURLAsync(new Regex(".*Home/Form"));
