5b986cf299fc1ddc4828861b2c1705e55383d3b6
MgKyaw <mgkyaw20112@gmail.com>
2025-09-14 14:36:51 +0700

    feat: Create new nunit test project

---
+++ ContactForm.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactForm.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
